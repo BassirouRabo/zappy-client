5b1aeba8ed1a9fa05403e4abec4f47a9510993c0
Bassirou Rabo Hima (brabo-hi) <smalllamartin@gmail.com>
2018-06-22 06:05:27 -0700

    jar

--- src/main/kotlin/start.kt
+++ src/main/kotlin/start.kt
@@ -1,34 +1,25 @@
-
 import java.util.*
+import kotlin.system.exitProcess
 
-var num = 1000
-
+fun printUsage() {
+	println("Client Usage: ./client \n\t-n <team> \n\t-p <port> \n\t[-h <hostname>]")
+	exitProcess(-1)
+}
 
 fun main(args: Array<String>) {
 
-	val runnable = Runnable {
-		Thread.sleep(2000)
-		println("Hello ${Thread.currentThread().name}")
-	}
+	if (args.size != 3)
+		printUsage()
 
-	runnable.run()
-
-	val thread = Thread(runnable)
-	thread.start()
-	/*println("##")
-	for (i in 1..20) {
-		Thread(
-			{
-				val code = num++
-				try {
-					val proc = ProcessBuilder("java", "-jar", "../../../build/libs/client-1.0.jar", "red", "4242", "localhost").start()
-					Scanner(proc.inputStream).use {
-						while (it.hasNextLine()) println("$code: " + it.nextLine())
-					}
-				}catch (e: Exception) {
-					println("ERR ${e.message}")}
+	while (true) {
+		try {
+			val proc =  ProcessBuilder("java", "-jar", "../../../build/libs/client-1.0.jar", args[0], args[1], args[2]).start()
+			Scanner(proc.inputStream).use {
+				while (it.hasNextLine()) println(it.nextLine())
 			}
-		).start()
+			Thread.sleep(5000)
+		} catch (e: Exception) {
+			println("${e.message}")
+		}
 	}
-	println("##")*/
 }
